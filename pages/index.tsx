b2516c4320ca7148f11eeaead8f780b8067cc2cd
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2023-06-29 22:13:15 +0300

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,22 @@
+import { getBooks } from '@/api/getBooks';
+import { AppBar } from '@/widgets/AppBar';
 import { HomePage } from '@/widgets/HomePage';
 import { PageLayout } from '@/widgets/PageLayout';
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from 'next';
 
-export default function Home() {
+export default function Home({ books }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <PageLayout
       title="Книги по графическому дизайну и типографике"
       description="Каталог книг по графическому дизайну и типографике. Распродажа личной коллекции."
+      appBar={<AppBar />}
     >
-      <HomePage />
+      <HomePage books={books} />
     </PageLayout>
   );
 }
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+  const { data: bookMocks } = await getBooks();
+  return { props: { books: bookMocks } };
+};
