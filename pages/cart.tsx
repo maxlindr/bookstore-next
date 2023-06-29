7232a372404760001664a338fc0448e8143a93f5
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2023-06-29 22:12:20 +0300

    cart page

---
+++ pages/cart.tsx
@@ -0,0 +1,9 @@
+import { PageLayout } from '@/widgets/PageLayout';
+
+export default function Page() {
+  return (
+    <PageLayout title="Книги по графическому дизайну и типографике — Корзина" description="Корзина">
+      Корзина
+    </PageLayout>
+  );
+}
