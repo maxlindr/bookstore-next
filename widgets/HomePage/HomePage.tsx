7db74862cf271d8e2896b9c6f1464b5680f957b8
Maxim Timofeev <hallo.bonjour.salut@gmail.com>
2023-07-01 18:48:00 +0300

    HomePage

--- widgets/HomePage/HomePage.tsx
+++ widgets/HomePage/HomePage.tsx
@@ -1,13 +1,24 @@
+import { IBook } from '@/entities/IBook';
+import { BooksList } from '../BooksList';
 import styles from './HomePage.module.scss';
 
-export const HomePage = () => {
+interface IProps {
+  books: IBook[];
+}
+
+export const HomePage = ({ books }: IProps) => {
   return (
-    <>
+    <div className={styles.root}>
       <h1 className={styles.title}>
         Книги <br />
-        по&nbsp;графическому дизайну и&nbsp;типографике
+        по&nbsp;графическому дизайну <br />
+        и&nbsp;типографике
       </h1>
       <p className={styles.subhead}>Распродажа личной коллекции</p>
-    </>
+
+      <div className={styles.listWrapper}>
+        <BooksList books={books} />
+      </div>
+    </div>
   );
 };
